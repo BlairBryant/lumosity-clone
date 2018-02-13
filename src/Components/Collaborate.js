da00ba92698aaf305395711f0e21db98cb657cdf
Blair <blairslc@gmail.com>
2018-02-12 23:20:38 -0700

    updated Collaborate Component

--- src/Components/Collaborate.js
+++ src/Components/Collaborate.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import collaborateImg from '../Images/collaborate.png'
+import transformCheck from '../Images/transformCheck.png'
 
 class Collaborate extends Component {
     render() {
       return (
         <div className="collaborate">
-  
+			<h2>We collaborate with the scientific community</h2>
+			<h5>We work with 100+ researchers worldwide, many of whom we give free access to our brain training tools — helping them research new areas in human cognition.</h5>
+			<img src={collaborateImg} alt="collaborateImg"/>
+			<div>
+				<section>
+					<img src={transformCheck} alt="checkImg"/>
+					<li>Free access to Lumosity training</li>
+				</section>
+				<section>
+					<img src={transformCheck} alt="checkImg"/>
+					<li>Online cognitive assessments</li>
+				</section>
+				<section>
+					<img src={transformCheck} alt="checkImg"/>
+					<li>Guidance on data analysis</li>
+				</section>
+				<section>
+					<img src={transformCheck} alt="checkImg"/>
+					<li>Online surveys for participants</li>
+				</section>
+			</div>
+			<h4>Are you a researcher interested in collaborating with us? Learn More</h4>
         </div>
       );
     }

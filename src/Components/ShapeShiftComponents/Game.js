7167bbc039db5f1702d2b978712db51e1979e4a1
Blair <blairslc@gmail.com>
2018-03-08 10:38:55 -0700

    Added preGame page and other things

--- src/Components/ShapeShiftComponents/Game.js
+++ src/Components/ShapeShiftComponents/Game.js
@@ -6,18 +6,41 @@ export default class Game extends Component {
         super()
 
         this.state = {
-
+            playingGame: false
         }
     }
     render() {
+        const { playingGame } = this.state
         return (
             <div className='game'>
                 <div className='gameAndInfoContainer'>
                     <section className='gameBorder'>
-                        <section className='shapeShift'>
 
-                        </section>
+                        {
+                            playingGame
+                                ?
+                                <section className='shapeShift'>
+                                    <div className='colorDiv' id='leftColor'></div>
+                                    <div className='colorDiv' id='rightColor'></div>
+
+                                </section>
+                                :
+                                <section className='preGame'>
+                                    <h2>Shape Shift</h2>
+                                    <h5>Divide your attention across changing colors, shapes, and positions</h5>
+                                    <div className='preGameShape'>Triangle</div>
+                                    <div className='gameButtonsHolder'>
+                                        <button className='logInButton' id='howToPlay'>How to Play</button>
+                                        <button className='getStartedButton' onClick={() => this.setState({ playingGame: true })}>Play</button>
+                                    </div>
+                                </section>
+
+                        }
                     </section>
+
+
+
+
                     <section className='attentionBlurb'>
                         <h5>Attention Division</h5>
                         <p className='attentionLine'></p>
